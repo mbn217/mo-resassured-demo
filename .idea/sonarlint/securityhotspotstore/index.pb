2af619037a3b792302b95fb686467025f6b05832
Mohamed Nheri <mohamed.nheri@bigbear.ai>
2024-03-26 10:21:44 -0400

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+Q
+!src/test/java/tests/Example1.java,6\b\6bec581dd7dfa90a338c12103896477c6d0673f0
+B
+Notes_Commands.txt,a\4\a452fe13ff637e09d52fa960d6daae23e2596d18
---
+++ src/test/java/tests/Example1.java
@@ -0,0 +1,20 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+    @Test
+    public void test1() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getTime());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getHeader("Content-Type"));
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
